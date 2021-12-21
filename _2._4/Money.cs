51953dbac80f6629d8daffabd0c8f3005c582f26
User23932344 <olegmatusevitch@gmail.com>
2021-12-21 15:41:46 +0300

    Добавьте файлы проекта.

---
+++ _2._4/Money.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+
+namespace _2._4
+{
+    class Money
+    {
+        int rub;
+        int cop;
+        int Rub
+        {
+            get { return rub; }
+            set
+            {
+                if (value >= 0)
+                {
+                    rub = value;
+                }
+                else
+                {
+                    Console.WriteLine("Negative value");
+                }
+            }
+        }
+        int Cop
+        {
+            get { return cop; }
+            set
+            {
+                if (value >= 0)
+                {
+                    cop = value;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect value");
+                }
+            }
+        }
+        public Money(int rub, int cop)
+        {
+            this.Rub = rub;
+            this.Cop = cop;
+        } 
+        public Money() { }
+        static public Money NumberDivision(int value)
+        {
+            return new Money { Rub = value / 100, Cop = value % 100 };
+        }
+        static public int RubToCop(Money money)
+        {
+            return money.Rub * 100 + money.Cop;
+        }
+        public new string ToString() 
+        {
+            double value = Rub + ((double)Cop / 100);
+            return Convert.ToString(value);
+        }
+        static public Money operator +(Money money1, Money money2) 
+        {
+            int value1 = RubToCop(money1);
+            int value2 = RubToCop(money2);
+
+            return NumberDivision(value1 + value2);
+        }
+        static public bool operator >(Money money1, Money money2)
+        {
+            int value1 = RubToCop(money1);
+            int value2 = RubToCop(money2);
+            if (value1 > value2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static public bool operator <(Money money1, Money money2)
+        {
+            int value1 = RubToCop(money1);
+            int value2 = RubToCop(money2);
+            if (value1 < value2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
---
+++ _2._4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money money1 = new Money(3, 5);
+            Console.WriteLine(Money.RubToCop(money1));
+            Console.WriteLine(money1.ToString());
+            Money money2 = new Money(5, 70);
+            Money money3 = money1 + money2;
+            Console.WriteLine(money3.ToString());
+            Console.WriteLine(money1 > money2);
+            Console.WriteLine(money1 < money2);
+        }
+    }
+}
